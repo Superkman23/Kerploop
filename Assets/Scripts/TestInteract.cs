360a1417632ac8aa19c5dd4966fd5d734a6df6fd
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-02-01 19:31:36 +0000

    :art: :sparkles: Redo TestScene, Add name and info to scripts, make TestInteract editable

--- Assets/Scripts/TestInteract.cs
+++ Assets/Scripts/TestInteract.cs
@@ -1,13 +1,19 @@
 ﻿/*
  * TestInteract.cs
- * Created by: #AUTHOR#
- * Created on: #CREATIONDATE# (dd/mm/yy)
- * Created for: #PURPOSE#
+ * Created by: Ambrosia
+ * Created on: 1/2/2020 (dd/mm/yy)
+ * Created for: showing off the power of the Interactable interface
  */
 
 using UnityEngine;
 
 public class TestInteract : MonoBehaviour, Interactable
 {
-    public void OnInteractStart() => Debug.Log("Bruhzej");
+    [SerializeField] Vector3 _Direction = Vector3.up;
+    [SerializeField] float _Force = 250;
+
+    public void OnInteractStart()
+    {
+        GetComponent<Rigidbody>().AddForce(_Direction * _Force);
+    }
 }
