46a854deb41c4a09a1441bfa161fe506cf979c28
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-02-01 03:02:27 +0000

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,28 +12,38 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float _MovementSpeed = 5;
+    [SerializeField] [Range(0, 1)] private float _InputCutoff = 0.5f;
     
     [Header("Gravity")]
     [SerializeField] private float _GravityMultiplier = 1;
 
     private Rigidbody _Rigidbody;
+    private Camera _Camera;
 
-    void Awake()
+    private void Awake()
     {
         _Rigidbody = GetComponent<Rigidbody>();
+        _Camera = Camera.main;   
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        var input = new Vector3(Input.GetAxis("Horizontal") * _MovementSpeed, _Rigidbody.velocity.y ,Input.GetAxis("Vertical") * _MovementSpeed);
-        if (input.x == 0 && input.z == 0)
+        var mDirection = new Vector3(Input.GetAxis("Horizontal"), _Rigidbody.velocity.y ,Input.GetAxis("Vertical"));
+        if (mDirection.x == 0 && mDirection.z == 0)
         {
-            if (input.y != 0) // See if we are falling
+            if (mDirection.y != 0) // See if we are falling
                 _Rigidbody.velocity = (Vector3.down * _Rigidbody.velocity.y) * _GravityMultiplier;
 
             return; // exit out early
         }
         
-        _Rigidbody.velocity = transform.TransformDirection(input);
+        _Rigidbody.velocity = ApplyRelativeCamera(mDirection) * _MovementSpeed;
+    }
+
+    private Vector3 ApplyRelativeCamera(Vector3 direction)
+    {
+        var tDirection = _Camera.transform.TransformDirection(direction);
+        tDirection.y = direction.y;
+        return tDirection.normalized;
     }
 }
