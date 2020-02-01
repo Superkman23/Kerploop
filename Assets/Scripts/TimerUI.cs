56e38a52bba07622f1df54332aa21c2c065a0a1b
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-01-31 22:28:10 -0500

    Started Timer

---
+++ Assets/Scripts/TimerUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class TimerUI : MonoBehaviour
+{
+
+    private Text _Text;
+    [SerializeField] private float _SecondsPerMinute;
+    private float _Time;
+
+
+
+    void Awake()
+    {
+        _Text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Time += Time.deltaTime;
+        int hours = Mathf.FloorToInt(_Time / (_SecondsPerMinute * 60));
+        int minutes = Mathf.FloorToInt((_Time - hours) / _SecondsPerMinute); 
+
+        _Text.text = hours + ":" + minutes + "AM";
+
+    }
+}
