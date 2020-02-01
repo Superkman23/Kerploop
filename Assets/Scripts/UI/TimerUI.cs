d28d8082b650c1b7a1a58c53bc364619f4602d80
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-02-01 14:50:47 -0500

    Loaded TimerUI with comments

--- Assets/Scripts/UI/TimerUI.cs
+++ Assets/Scripts/UI/TimerUI.cs
@@ -3,9 +3,11 @@ using TMPro;
 
 public class TimerUI : MonoBehaviour
 {
-    [SerializeField] int _SecondsPerHour = 60;
+    [SerializeField] int _IRLSecondsPerHour = 60;
+    [SerializeField] int _StartingHour = 12;
+
     [SerializeField] bool _IsAm = false;
-    bool _DidChange;
+    bool _DidChange = true;
 
     TextMeshProUGUI _Text;
     float _Time;
@@ -13,30 +15,39 @@ public class TimerUI : MonoBehaviour
     private void Awake()
     {
         _Text = GetComponent<TextMeshProUGUI>();
-        _Time = _SecondsPerHour * 12;
+        //Starts the game at the specified hour in the day
+        _Time = _IRLSecondsPerHour * _StartingHour;
     }
 
     private void Update()
     {
+        //Adds time to the timer
         _Time += Time.deltaTime;
         float timeLeft = _Time;
 
-        int hours = Mathf.FloorToInt(_Time / _SecondsPerHour);
-        timeLeft -= Mathf.FloorToInt(hours * _SecondsPerHour);
-        int minutes = Mathf.FloorToInt(timeLeft / (_SecondsPerHour / 60));
+        //Uses total time to calculate hours
+        int hours = Mathf.FloorToInt(_Time / _IRLSecondsPerHour);
+        //Removes the hours to only use the extra time
+        timeLeft -= Mathf.FloorToInt(hours * _IRLSecondsPerHour);
+        //Uses the remaining time to calculate the minutes
+        int minutes = Mathf.FloorToInt(timeLeft / (_IRLSecondsPerHour / 60));
 
-        if (hours > 12)
+        //Set's the hours to 1 when there are more than 12 hours (12:59 AM to 1:00 AM)
+        if (hours > 12) 
         {
-            _DidChange = false;
-            _Time -= 12 * _SecondsPerHour;
+            _DidChange = false; //Prepares for the next AM to PM swap
+            _Time -= 12 * _IRLSecondsPerHour; //Removes the extra 12 hours
         }
 
+
+        //Swaps from AM to PM when it reaches 12:00 (11:59 AM to 12:00 PM)
         if (hours == 12 && minutes == 0 && !_DidChange)
         {
-            _IsAm = !_IsAm;
-            _DidChange = true;
+            _IsAm = !_IsAm; //Switches from AM to PM
+            _DidChange = true; //Makes the if statement only trigger once each time it reaches 12
         }
 
+        //Writes the text
         _Text.text = hours + ((minutes < 10) ? ":0" : ":") + minutes;
         _Text.text += _IsAm ? " AM" : " PM"; 
     }
