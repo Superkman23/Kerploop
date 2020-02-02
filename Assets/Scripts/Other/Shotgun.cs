efc4fbf0650bd15689a5b57e8b00fc0b8db6e031
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-02-02 00:56:11 -0500

    Implemented shotgun spread

--- Assets/Scripts/Other/Shotgun.cs
+++ Assets/Scripts/Other/Shotgun.cs
@@ -1,16 +1,41 @@
 ﻿/*
  * Shotgun.cs
- * Created by: #AUTHOR#
- * Created on: #CREATIONDATE# (dd/mm/yy)
- * Created for: #PURPOSE#
+ * Created by: Kaelan Bartlett
+ * Created on: 2/2/20 (dd/mm/yy)
+ * Created for: Shotguns
  */
 
 using UnityEngine;
 
 public class Shotgun : Gun
 {
+    [SerializeField] int _PelletsPerShot = 10;
+    [SerializeField] float _Spread;
+
 	public override void Shoot()
 	{
-		// DO STUFF HERE
-	}
+        int pelletsleft = _PelletsPerShot;
+        // Play the audio of the gun shooting
+        _AudioSource.PlayOneShot(_ShootNoise, _ShootNoiseVolume);
+        RaycastHit hit;
+
+        while (pelletsleft > 0)
+        {
+            float spreadZ = Random.Range(-_Spread, _Spread);
+            float spreadY = Random.Range(-_Spread, _Spread);
+            Vector3 spread = new Vector3(0, spreadY, spreadZ);
+
+            Debug.DrawRay(_MainCamera.transform.position, _MainCamera.transform.forward + spread,Color.green,2);
+            if (Physics.Raycast(_MainCamera.transform.position, _MainCamera.transform.forward + spread, out hit, _BulletMaxDistance))
+            {
+                var hitRB = hit.rigidbody;
+                if (hitRB != null)
+                {
+                    hitRB.AddForce(_MainCamera.transform.forward * _RigidbodyForce, ForceMode.Impulse);
+                }
+            }
+            pelletsleft--;
+        }
+    }
+
 }
