2a016a29a75a1b30a8172482e2479a47d9fa2700
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-02-02 16:42:37 +0000

    :art: Implement health

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,33 +6,49 @@
  */
 
 using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour, IHealth
 {
+	[Header("Components")]
+	[SerializeField] TextMeshProUGUI _HealthText;
+	List<CanvasRenderer> _HealthDisplayBackground = new List<CanvasRenderer>();
+
 	[Header("Settings")]
 	[SerializeField] KeyCode _DropButton = KeyCode.F;
 	[SerializeField] float _ThrowForce = 5;
+    [SerializeField] int _LocalMaxHealth = 100;
+	int _LocalHealth = 0;
 
 	Camera _MainCamera;
     Rigidbody _Rigidbody;
     [HideInInspector] public GameObject _CurrentGun = null;
 
-
-	int _LocalHealth = 0;
     public int _Health 
 	{ 
 		get => _LocalHealth; 
 		set { if (value > _LocalMaxHealth) value = _LocalMaxHealth;
 			_LocalHealth = value; }
 	}
-    int _LocalMaxHealth = 0;
 	public int _MaxHealth { get => _LocalMaxHealth; set => _LocalMaxHealth = value; }
 
     private void Awake() 
 	{
+		_LocalHealth = _LocalMaxHealth;
+
 		_MainCamera = Camera.main;
         _Rigidbody = GetComponent<Rigidbody>();
         Globals._MainPlayer = this;
+
+		foreach (Transform child in _HealthText.transform)
+		{
+			var renderer = child.GetComponent<CanvasRenderer>();
+			if (renderer != null)
+			{
+				_HealthDisplayBackground.Add(renderer);
+			}
+		}
 	}
 
 	private void Update() 
@@ -40,7 +56,13 @@ public class Player : MonoBehaviour, IHealth
 		if (Input.GetKeyDown(_DropButton))
 		{
 			DropWeapon();
+		} 
+
+		foreach (var backgroundObj in _HealthDisplayBackground)
+		{
+			backgroundObj.SetColor(Color.Lerp(Color.red, Color.green, _Health / 100));
 		}
+		_HealthText.text = _Health.ToString();
 	}
 
 	public void DropWeapon()
