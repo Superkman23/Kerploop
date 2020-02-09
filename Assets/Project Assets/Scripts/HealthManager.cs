ed8ea393a215602dc793f3006f0f4ce36693a6e4
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-02-09 18:51:01 -0500

    Implemented Health Bar functionality by doing this the player now has health. press y to subtract 1 health from the player

---
+++ Assets/Project Assets/Scripts/HealthManager.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * HealthManager.cs
+ * Created by: Kaelan Bartlett
+ * Created on: 3/2/2020 (dd/mm/yy)
+ * Created for: Managing health
+ */
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] public float _MaxHealth;
+    [HideInInspector] public float _CurrentHealth;
+    [HideInInspector] public float _HealthPercent;
+    [SerializeField] bool _IsPlayer = false;
+
+
+    private void Awake()
+    {
+        _CurrentHealth = _MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _HealthPercent = _CurrentHealth / _MaxHealth;
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            _CurrentHealth--;
+        }
+        //Health is at 0 so the object has died
+        if (_CurrentHealth <= 0)
+        {
+            if (_IsPlayer)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Project Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] HealthManager _HealthManager;
+    [SerializeField] RectTransform _Bar;
+    Vector2 _FullSize;
+
+    [Header("Misc")]
+    [SerializeField] [Range(0,1)] float _ChangeSpeed;
+
+
+    void Awake()
+    {
+        _FullSize = _Bar.sizeDelta;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector2 targetSize = new Vector2(_FullSize.x * _HealthManager._HealthPercent, _FullSize.y);
+        Debug.Log(targetSize);
+        _Bar.sizeDelta = Vector2.Lerp(_Bar.sizeDelta, targetSize, _ChangeSpeed);
+        _Bar.anchoredPosition = new Vector2((_Bar.sizeDelta.x - _FullSize.x) / 2, 0);
+    }
+}
