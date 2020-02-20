bf91bd0aa2b4cf5fe3f1c3577ff362f95b54db70
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-02-20 00:25:43 -0500

    Improved healthbar

--- Assets/Project Assets/Scripts/UI/HealthBar.cs
+++ Assets/Project Assets/Scripts/UI/HealthBar.cs
@@ -4,6 +4,7 @@
  * Created on: 3/2/2020 (dd/mm/yy)
  * Created for: Drawing a healthbar on screen
  */
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,14 +13,14 @@ public class HealthBar : MonoBehaviour
     [Header("Components")]
     [SerializeField] HealthManager _HealthManager;
     [SerializeField] Image _Bar;
+    [SerializeField] Text _Amount;
 
     [Header("Misc")]
     [SerializeField] [Range(0,1)] float _ChangeSpeed;
 
-
-    // Update is called once per frame
     void Update()
     {
         _Bar.fillAmount = Mathf.Lerp(_Bar.fillAmount, _HealthManager._HealthPercent, _ChangeSpeed);
+        _Amount.text = _HealthManager._CurrentHealth.ToString();
     }
 }
