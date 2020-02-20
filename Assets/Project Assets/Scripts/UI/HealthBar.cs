3c550f92b5ac9e85e9e05c0e4d8402ea987359a8
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-02-20 00:13:31 -0500

    Improved healthbar texture

--- Assets/Project Assets/Scripts/UI/HealthBar.cs
+++ Assets/Project Assets/Scripts/UI/HealthBar.cs
@@ -5,28 +5,21 @@
  * Created for: Drawing a healthbar on screen
  */
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField] HealthManager _HealthManager;
-    [SerializeField] RectTransform _Bar;
-    Vector2 _FullSize;
+    [SerializeField] Image _Bar;
 
     [Header("Misc")]
     [SerializeField] [Range(0,1)] float _ChangeSpeed;
 
 
-    void Awake()
-    {
-        _FullSize = _Bar.sizeDelta;
-    }
-
     // Update is called once per frame
     void Update()
     {
-        Vector2 targetSize = new Vector2(_FullSize.x * _HealthManager._HealthPercent, _FullSize.y);
-        _Bar.sizeDelta = Vector2.Lerp(_Bar.sizeDelta, targetSize, _ChangeSpeed);
-        _Bar.anchoredPosition = new Vector2((_Bar.sizeDelta.x - _FullSize.x) / 2, 0);
+        _Bar.fillAmount = Mathf.Lerp(_Bar.fillAmount, _HealthManager._HealthPercent, _ChangeSpeed);
     }
 }
