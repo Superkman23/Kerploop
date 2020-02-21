49ddd2e426d4244988432bfed48768fbd3242672
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-02-21 00:09:51 -0500

    Created a pistol enemy prefab, AI now drops it's gun on death

--- Assets/Project Assets/Scripts/Enemy/AI.cs
+++ Assets/Project Assets/Scripts/Enemy/AI.cs
@@ -11,20 +11,21 @@ using UnityEngine.AI;
 public class AI : MonoBehaviour
 {
     [Header("Components")]
-    NavMeshAgent _Agent;
     [SerializeField] Gun _Gun;
-
-    Vector3 _TargetPosition;
     [SerializeField] GameObject _Player;
     [SerializeField] GameObject _Eyes;
+    NavMeshAgent _Agent;
 
     [Header("Settings")]
     [SerializeField] float _ViewDistance;
     [SerializeField] float _MaxShootDistance;
-    [SerializeField] float _TargetSpacing;
     [SerializeField] float _RotationSpeed;
-	
-	void Awake()
+    [SerializeField] float _ShotDelay; // To prevent the AI from spamming non automatic guns 
+    float _TimeTillNextShot;
+
+    Vector3 _TargetPosition;
+
+    void Awake()
 	{
         _Agent = GetComponent<NavMeshAgent>();
         _TargetPosition = transform.position;
@@ -46,17 +47,21 @@ public class AI : MonoBehaviour
 
         if(Vector3.Distance(transform.position, _Player.transform.position) <= _MaxShootDistance)
         {
-            if (_Gun._CanShoot && _Gun._CurrentInClip > 0)
+            if (_Gun._CanShoot && _Gun._CurrentInClip > 0 && _TimeTillNextShot <= 0)
             {
                 _Gun.Shoot();
+                _TimeTillNextShot = _ShotDelay;
             }
+            if (_TimeTillNextShot > 0)
+                _TimeTillNextShot -= Time.deltaTime;
         }
 
         if ( _Gun._CurrentInClip <= 0)
         {
             _Gun.StartReloading();
         }
-        
+
+
     }
     private void LookAtPlayer()
     {
@@ -64,5 +69,12 @@ public class AI : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, _RotationSpeed * Time.deltaTime);
     }
+
+    public void OnDestroy()
+    {
+        _Gun.Drop();
+    }
+
+
 
 }
