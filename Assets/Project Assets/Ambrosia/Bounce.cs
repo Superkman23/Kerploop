8aaff798dbaa8f915357d4b9fd19dfe3625556c7
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-02-21 00:27:23 +0000

    Add Ambrosia's scene

---
+++ Assets/Project Assets/Ambrosia/Bounce.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Bounce.cs
+ * Created by: Ambrosia
+ * Created on: #CREATIONDATE# (dd/mm/yy)
+ * Created for: #PURPOSE#
+ */
+
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] float _Force = 50;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var rb = collision.transform.GetComponent<Rigidbody>();
+        if (rb == null)
+            return;
+        rb.AddForce(Vector3.up * _Force);
+    }
+}
---
+++ Assets/Project Assets/Ambrosia/OnTriggerResetScene.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * OnTriggerResetScene.cs
+ * Created by: Ambrosia
+ * Created on: #CREATIONDATE# (dd/mm/yy)
+ * Created for: #PURPOSE#
+ */
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnTriggerResetScene : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") == false)
+            return;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
