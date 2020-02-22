0145794a9b58fb83db130ddfd22b460c32dd2fae
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-02-22 02:50:20 +0000

    Do some more Ambrosia_Scene

--- Assets/Project Assets/Scenes/Ambrosia's_Scene/Enhancer.cs
+++ Assets/Project Assets/Scenes/Ambrosia's_Scene/Enhancer.cs
@@ -3,20 +3,39 @@
  * Created by: Ambrosia
  */
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 public class Enhancer : MonoBehaviour
 {
-	[Header("Settings")]
-	[SerializeField] float _LifeTime = 5;
+	[Header("First Trigger Variables")]
+
+	[SerializeField] bool _FirstTrigger = false;
+	// Chromatic Aberration Film Grain
+	[SerializeField] Volume _CAFGVolume;
+	[SerializeField] bool _Enabling = true;
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag("Player"))
 		{
-			
+			if (_FirstTrigger)
+			{
+				_CAFGVolume.profile.TryGet(out ChromaticAberration ca);
+				ca.active = _Enabling;
+				_CAFGVolume.profile.TryGet(out FilmGrain fg);
+				fg.active = _Enabling;
+
+				_CAFGVolume.profile.TryGet(out WhiteBalance wb);
+				wb.active = !_Enabling;
+				_CAFGVolume.profile.TryGet(out LensDistortion ld);
+				ld.active = !_Enabling;
+				_CAFGVolume.profile.TryGet(out LiftGammaGain lgg);
+				lgg.active = !_Enabling;
+
+				Destroy(gameObject);
+			}
 		}
 	}
 }
