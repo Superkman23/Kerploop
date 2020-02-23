51a8d90cbfa0cacb0eacef5bf203b7d26e596f02
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-02-23 15:45:43 -0500

    Added a red tint whenever you take damage

---
+++ Assets/Project Assets/Scripts/UI/DamageEffect.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * DamageEffect.cs
+ * Created by: Kaelan 
+ * Created on: 23/2/2020 (dd/mm/yy)
+ * Created for: Tinting the screen red upon taking damage
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+public class DamageEffect : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] HealthManager _Manager;
+    Image _Image;
+
+    [Header("Settings")]
+    [SerializeField] float _EffectStrength;
+    [SerializeField] float _ChangeSpeed;
+    public float _LastHealth;
+
+	private void Awake()
+	{
+        _Image = GetComponent<Image>();
+        _Image.color = new Color(_Image.color.r, _Image.color.g, _Image.color.b, 0);
+    }
+    private void Start()
+    {
+        _LastHealth = _Manager.GetHealth();
+    }
+
+    private void LateUpdate()
+	{
+        float targetOpacity = _Manager.GetHealth() < _LastHealth ? 
+            (_LastHealth - _Manager.GetHealth()) * _EffectStrength : 
+            Mathf.Lerp(_Image.color.a, 0, _ChangeSpeed);
+
+        _LastHealth = _Manager.GetHealth();
+        _Image.color = new Color(_Image.color.r, _Image.color.g, _Image.color.b, targetOpacity);
+    }
+}
